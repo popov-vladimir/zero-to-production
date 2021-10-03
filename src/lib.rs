b5ce135930ff45204d059f97b368d93e7132662c
vladimir popov <popov.v.aleksandrovich@sberbank.ru>
2021-10-04 01:50:03 +0300

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,33 @@
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{web, App, HttpResponse, HttpServer,HttpRequest};
+use actix_web::http::{StatusCode};
+use actix_web::body::Body;
 use std::net::TcpListener;
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String
+}
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+async fn greet(req: HttpRequest) -> HttpResponse {
+    let name = req.match_info().get("name").unwrap_or("world");
+    HttpResponse::with_body(StatusCode::OK, Body::from(format!("hello,{}",name)))
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+    let server = HttpServer::new(|| App::new()
+    .route("/health_check", web::get().to(health_check))
+    .route("/greet/{name}", web::get().to(greet))
+    .route("/subscriptions", web::post().to(subscribe))
+)
         .listen(listener)?
         .run();
     Ok(server)
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -29,3 +29,51 @@ async fn health_check_works() {
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
+
+#[actix_rt::test]
+async fn subscribe_returns_200_if_valid_data() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let body = "name=test&email=test%40gmail.com";
+
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("failed to send request");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[actix_rt::test]
+async fn subscribe_returns_400_if_missing_data() {
+    let app_address = spawn_app();
+
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("email=test$%40gmail.com", "missing a name"),
+        ("name=name", "missing and email"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("failed to send request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail when payload was {}",
+            error_message
+        )
+    }
+}
