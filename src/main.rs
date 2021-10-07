0a064a19f4d47818e62ebafe6c52089bb69e3d98
vladimir popov <popov.v.aleksandrovich@sberbank.ru>
2021-10-07 20:47:22 +0300

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -7,5 +8,8 @@ async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("failed to get configuration");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("failed to connect to database");
+    run(listener,connection)?.await
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,11 +1,20 @@
+use chrono::Utc;
+use sqlx::PgConnection;
 use actix_web::{web, HttpResponse};
-
+use uuid::Uuid;
 #[derive(serde::Deserialize)]
 pub struct FormData {
     email: String,
-    name: String
+    name: String,
 }
 
-pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+pub async fn subscribe(_form: web::Form<FormData>,
+     _connection: web::Data<PgConnection>) -> HttpResponse {
+    
+    sqlx::query!(r#"insert into subscriptions(id, email, name, subscribed_at) values($1,$2,$3,$4)"#,
+    Uuid::new_v4(), _form.email, _form.name, Utc::now()
+)
+    .execute(_connection.get_ref())
+    .expect("failed to insert subscription");
+        HttpResponse::Ok().finish()
 }
